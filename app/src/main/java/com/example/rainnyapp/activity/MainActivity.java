17604f9ceb2246bb9dde89e5107526ad6e40ba4d
Quang Trung <quangtrung54677@gmail.com>
2025-08-22 22:10:16 +0700

    create fragment and add UI register

--- app/src/main/java/com/example/rainnyapp/activity/MainActivity.java
+++ app/src/main/java/com/example/rainnyapp/activity/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.rainnyapp.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.rainnyapp.R;
+import com.example.rainnyapp.fragment.HomeFragment;
+import com.example.rainnyapp.fragment.NotificationFragment;
+import com.example.rainnyapp.fragment.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +31,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNavigationView, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(v.getPaddingLeft(), v.getPaddingTop(), v.getPaddingRight(), systemBars.bottom);
+            return insets;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(v.getPaddingLeft(), v.getPaddingTop(), v.getPaddingRight(), systemBars.bottom);
+            return insets;
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+
+                if (itemId == R.id.nav_home) {
+                    loadFragment(new HomeFragment());
+                    return true;
+                } else if (itemId == R.id.nav_notifications) {
+                    loadFragment(new NotificationFragment());
+                    return true;
+                } else if (itemId == R.id.nav_add) {
+                    return true;
+                } else if (itemId == R.id.nav_favorites) {
+                    return true;
+                } else if (itemId == R.id.nav_profile) {
+                    loadFragment(new ProfileFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Load Fragment Home khi khoi dong
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        }
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.commit();
     }
 }
